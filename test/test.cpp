9515af7f092282ce8e6dcfd8c4cf6fd01ce7cb87
JJ Luo <asdw1212@126.com>
2020-09-07 15:54:21 +0800

    MI (Windows Management Infrastructure) testing code

--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,9 @@
 #include <cstdio>
 #include <cstdlib>
 #include "..\core\core.h"
+#include <mi.h>
+#include <locale.h>
+#pragma comment(lib, "mi.lib")
 
 HWMON_INFO hi;
 
@@ -83,6 +86,8 @@ int main(int argc, char** argv)
 		hi.msex.ullAvailExtendedVirtual / 1024
 	);
 
+	printf("\n\n");
+
 	HWMON_AllocCpus(&hi);
 /*
 	while (TRUE)
@@ -103,5 +108,134 @@ int main(int argc, char** argv)
 */
 
 	HWMON_FreeCpus(&hi);
+
+	//MI (Windows Management Infrastructure) Test
+
+	setlocale(LC_ALL, "chs");
+
+	MI_Application miApp = MI_APPLICATION_NULL;
+	MI_Result miResult = MI_RESULT_OK;
+	MI_Session miSession = MI_SESSION_NULL;
+	MI_Operation miOp = MI_OPERATION_NULL;
+	
+	miResult = MI_Application_Initialize(
+		0,
+		NULL,
+		NULL,
+		&miApp);
+
+	if (miResult != MI_RESULT_OK)
+	{
+		printf("MI_Application_Initialize failed. MI_Result: %ld\n", miResult);
+		return -1;
+	}
+
+	miResult = MI_Application_NewSession(
+		&miApp,
+		L"WMIDCOM", //Currently supported protocols are L"WMIDCOM" and L"WINRM"
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		&miSession);
+
+	if (miResult != MI_RESULT_OK)
+	{
+		printf("MI_Application_NewSession failed. MI_Result: %ld\n", miResult);
+		return -1;
+	}
+
+	MI_Session_EnumerateInstances(
+		&miSession,
+		0,
+		NULL,
+		L"root\\cimv2",
+		L"Win32_Process",
+		MI_FALSE,
+		NULL,
+		&miOp);
+
+	miResult = MI_RESULT_OK;
+	MI_Boolean moreResults;
+	const MI_Char* errorString = NULL;
+	MI_Uint32 instanceCount = 0;
+	MI_Instance* miInstance;
+	const MI_Instance* errorDetails = NULL;
+
+	do
+	{
+		MI_Operation_GetInstance(
+			&miOp,
+			(const MI_Instance**) &miInstance,
+			&moreResults,
+			&miResult,
+			&errorString,
+			&errorDetails);
+
+		if (miResult != MI_RESULT_OK)
+		{
+			printf("MI_Operation_GetInstance failed. MI_Result: %ld\n", miResult);
+			break;
+		}
+
+		if (miInstance)
+		{
+			MI_Value value;
+			MI_Type type;
+			MI_Uint32 flags;
+
+			miResult = MI_Instance_GetElement(
+				miInstance,
+				L"Name",
+				&value,
+				&type,
+				&flags,
+				NULL);
+
+			if (miResult != MI_RESULT_OK)
+			{
+				printf("MI_Instance_GetElement failed. MI_Result: %ld\n", miResult);
+				break;
+			}
+
+			wprintf(L"Process Name: %s\n", value.string);
+			instanceCount++;
+		}
+
+	} while (miResult == MI_RESULT_OK && moreResults == MI_TRUE);
+
+	if (miResult != MI_RESULT_OK)
+	{
+		wprintf(L"Operation failed: MI_Result=%ld, errorString=%s\n", miResult, errorString);
+	}
+	else
+	{
+		printf("Operation succeeded. Number of instances = %u\n", instanceCount);
+	}
+
+	miResult =  MI_Operation_Close(&miOp);
+
+	if (miResult != MI_RESULT_OK)
+	{
+		printf("MI_Operation_Close failed. MI_Result: %ld\n", miResult);
+		return -1;
+	}
+
+	miResult =  MI_Session_Close(&miSession, NULL, NULL);
+
+	if (miResult != MI_RESULT_OK)
+	{
+		printf("MI_Session_Close failed. MI_Result: %ld\n", miResult);
+		return -1;
+	}
+
+	miResult =  MI_Application_Close(&miApp);
+
+	if (miResult != MI_RESULT_OK)
+	{
+		printf("MI_Application_Close failed. MI_Result: %ld\n", miResult);
+		return -1;
+	}
+
 	return 0;
 }
